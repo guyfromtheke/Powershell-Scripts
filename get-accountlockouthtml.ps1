777aea03c1541f50ee8bb2a009d6632b927d5f21
guyfromtheke <kienjdu@gmail.com>
2021-04-03 20:46:57 +0300

    Create get-accountlockouthtml.ps1

---
+++ get-accountlockouthtml.ps1
@@ -0,0 +1,23 @@
+## Report for DC login Failures ##
+$failedevent = $null
+$Date= Get-date
+$dc = Read-Host 'What is the Domain Controller ?'
+$Report= "C:\auditreport.html"
+$HTML=@"
+<title>Failed Login Report for $dc</title>
+<style>
+BODY{background-color :LightBlue}
+</style>
+"@
+$failedevent = Get-Eventlog security -Computer $dc -InstanceId 4625 -After (Get-Date).AddDays(-7) |
+Select-Object TimeGenerated,ReplacementStrings |
+%
+ {
+    New-Object PSObject -Property @{
+    SourceComputer = $_.ReplacementStrings[13]
+    UserName = $_.ReplacementStrings[5]
+    SourceIPAddress = $_.ReplacementStrings[19]
+    Date = $_.TimeGenerated
+}
+}
+$failedevent | ConvertTo-Html -Property SourceComputer,UserName,SourceIPAddress,Date -head $HTML -body "< Out-File $Report Invoke-Expression C:\auditreport.html" 
